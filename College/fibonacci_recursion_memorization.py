9bc04a057de32edcb9c67b6692eb045793c45502
rabinp <poudelrabin2004@example.com>
2024-12-16 23:40:41 +0545

    memorization vs recursion

--- College/fibonacci_recursion_memorization.py
+++ College/fibonacci_recursion_memorization.py
@@ -36,7 +36,13 @@ def fibo_memorization(n):
     return table[n]
 
 
-ask_option = input("Try using \n1)recursion \n 2) Memorization")
+print("-" * 30)
+
+ask_option = int(
+    input(
+        "Calculate Fibonacci using \n 1)Recursion (!Reminder  Recursion Fails above 49) \n 2) Memorization \n 3)Compare Both\n"
+    )
+)
 user_input = int(input("Enter nth term :"))
 inital_time1 = time.time()
 result_1 = fibo_recursion(user_input)
@@ -45,8 +51,18 @@ time1 = time.time() - inital_time1
 inital_time2 = time.time()
 result_2 = fibo_memorization(user_input)
 time2 = time.time() - inital_time2
-print("-" * 30)
-print(f" Using recursion : {result_1} \n Time taken: {time1} seconds")
-print("-" * 30)
-print(f" Using memorization : {result_2} \n Time taken : {time2}")
-print("-" * 30)
+if ask_option == 1:
+    print("*Recursion")
+    result_1 = fibo_recursion(user_input)
+    print(f" Using recursion : {result_1} \n Time taken: {time1} seconds")
+elif ask_option == 2:
+    print("*Memorizaation")
+    result_2 = fibo_memorization(user_input)
+    print(f" Using memorization : {result_2} \n Time taken : {time2}")
+else:
+    print("*Comparison")
+    print("-" * 30)
+    print(f" Using recursion : {result_1} \n Time taken: {time1} seconds")
+    print("-" * 30)
+    print(f" Using memorization : {result_2} \n Time taken : {time2}")
+    print("-" * 30)
