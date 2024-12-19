78182ac8aab4e9e2d20e5388572b2f4dc299c838
rabinp <poudelrabin2004@example.com>
2024-12-19 16:25:52 +0545

    circularqueue

---
+++ College/lab_2/Queue_dequeue.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX 5
+struct CircularQueue {
+    int FRONT;
+    int REAR;
+    int DATA[MAX]
+};
+typedef struct CircularQueue CQ;
+
+int main()
+{
+    int choice;
+    printf("Choices:\n1. Enqueue\n2. Dequeue\n3. Exit\n");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+    case 1:{
+        break;
+    }
+    case2:{
+        break;
+    }
+    case3:{
+        break;
+    }
+}
+    return 0;
+}
