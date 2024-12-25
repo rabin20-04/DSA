8d2a365bac069f5b3a8cd6649d8b44f2d4d448f8
rabinp <poudelrabin2004@example.com>
2024-12-25 21:04:56 +0545

    tower of hanoi py

--- College/lab_3_assignment_3/tower_of_hanoi.py
+++ College/lab_3_assignment_3/tower_of_hanoi.py
@@ -1,10 +1,11 @@
 def tower_of_hanoi(num, source, destination, temporary):
     if num == 1:
-        print(f"Move Disc {num} from {source} to {destination}\n")
+        print(f"Move Disc {num} from {source} to {destination}")
         return
     tower_of_hanoi(num - 1, source, temporary, destination)
     print(f"Move Disc {num} from {source} to {destination}\n")
     tower_of_hanoi(num - 1, temporary, destination, source)
 
 
-tower_of_hanoi(5, "A", "C", "B")
+num = int(input("Enter the number of discs : "))
+tower_of_hanoi(num, "A", "C", "B ")
